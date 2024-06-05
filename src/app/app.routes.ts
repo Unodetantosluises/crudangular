d0ca5507b0249a565599225bd5854d1903515e1f
Luis Diaz <luisantoniodiaz97@gmail.com>
2024-06-05 06:32:48 -0600

    Update: routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { EditEmployeeComponent } from './components/edit-employee/edit-employee.component';
+import { ListEmployeeComponent } from './components/list-employee/list-employee.component';
+
 export const routes: Routes = [];
